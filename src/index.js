5c988d446f8eabfa70c114f4995de26376d45faa
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-06-18 15:33:58 +0200

    feat+refactor: add dead heat and seperate board component

--- src/index.js
+++ src/index.js
@@ -1,53 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HistoryButton } from "./Buttons/History";
-import { NewGameButton } from "./Buttons/NewGame";
-import "./index.css";
-
-function Square(props) {
-  return (
-    <button
-      className="square"
-      onClick={props.onClick}
-      style={{ color: props.value === "X" ? "LightPink" : "LemonChiffon" }}
-    >
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div className="board">
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
+import { HistoryButton } from "./components/Buttons/History";
+import { NewGameButton } from "./components/Buttons/NewGame";
+import { Board } from "./components/Board";
+import { calculateWinner } from "./helper";
+import "./styles/index.css";
 
 class Game extends React.Component {
   constructor(props) {
@@ -125,7 +82,9 @@ class Game extends React.Component {
     });
     let status;
     if (winner) {
-      status = `${winner} is winner!!`;
+      status = `${winner} is winner!`;
+    } else if (!current.squares.includes(null)) {
+      status = "Dead Heat";
     } else {
       status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
     }
@@ -152,27 +111,5 @@ class Game extends React.Component {
     );
   }
 }
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-// ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
