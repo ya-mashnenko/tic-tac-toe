d2de2664d37c1cb8e29035436e93f30d6a216b8e
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-06-18 15:35:26 +0200

    refactor: replace components to folders

---
+++ src/components/Board.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+      style={{ color: props.value === "X" ? "LightPink" : "LemonChiffon" }}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+export class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="board">
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
